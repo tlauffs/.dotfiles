5065c2f7d3694eac5e013268b02b2fa9a75412f7
tlauffs <timlauffs@gmail.com>
2025-02-09 00:39:53 +0100

    install script

--- install/install_tools_deb.sh
+++ install/install_tools_deb.sh
@@ -23,7 +23,7 @@ sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flat
 echo "Updating apt repositories and installing dependencies..."
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y curl git unzip gpg wget make gcc ripgrep fzf fd-find sudo
+sudo apt install -y curl git unzip gpg wget make gcc ripgrep fzf fd-find sudo ca-certificates
 # Install additional packages
 echo "Installing packages..."
 sudo apt install -y vim stow tmux 
@@ -54,11 +54,14 @@ curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh 
 
 # Docker
 # Add the official Docker repo
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo tee /etc/apt/keyrings/docker.asc > /dev/null
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
 # Install Docker engine and standard plugins
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras

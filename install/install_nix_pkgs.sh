fd8692949e65edd59eb00cde7620e7cd55dda281
tlauffs <timlauffs@gmail.com>
2025-02-09 02:23:41 +0100

    fixes

--- install/install_nix_pkgs.sh
+++ install/install_nix_pkgs.sh
@@ -4,7 +4,7 @@ nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
 nix-channel --update
 nix-env -iA nixpkgs.neovim nixpkgs.fish nixpkgs.starship nixpkgs.zoxide nixpkgs.lazydocker
 
-nix-env -iA nixpkgs.python39 nixpkgs.nodejs_23 nixpkgs.php84
+nix-env -iA nixpkgs.python39 nixpkgs.nodejs_23 nixpkgs.php84 nixpkgs.php84Packages.composer
 
 # change npm prefix
 mkdir -p "${HOME}/.npm/global"

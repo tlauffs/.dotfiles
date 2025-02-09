7c540735387eca8817fc7d7c8c70db3bd0392d3a
tlauffs <timlauffs@gmail.com>
2025-02-09 03:00:05 +0100

    fixes

--- install/install_nix_pkgs.sh
+++ install/install_nix_pkgs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# tools
 nix-env -iA nixpkgs.neovim nixpkgs.fish nixpkgs.starship nixpkgs.zoxide nixpkgs.lazydocker nixpkgs.fastfetch nixpkgs.bat nixpkgs.dust nixpkgs.tldr nixpkgs.btop nixpkgs.yazi nixpkgs.direnv 
-nix-env -iA nixpkgs.python314 nixpkgs.nodejs_23 nixpkgs.php84 nixpkgs.php84Packages.composer nixpkgs.cargo 
 
+# languages
+nix-env -iA nixpkgs.python314 nixpkgs.nodejs_23 nixpkgs.php84 nixpkgs.php84Packages.composer nixpkgs.cargo 
 # change npm prefix
 mkdir -p "${HOME}/.npm/global"
 npm config set prefix "${HOME}/.npm/global"

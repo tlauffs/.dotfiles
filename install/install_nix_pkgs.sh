10f215890de2608ad65bfaf2ebbdf4b9af1efb58
tlauffs <timlauffs@gmail.com>
2025-02-09 02:41:32 +0100

    fixes

--- install/install_nix_pkgs.sh
+++ install/install_nix_pkgs.sh
@@ -2,7 +2,7 @@
 
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
 nix-channel --update
-nix-env -iA nixpkgs.neovim nixpkgs.fish nixpkgs.starship nixpkgs.zoxide nixpkgs.lazydocker
+nix-env -iA nixpkgs.neovim nixpkgs.fish nixpkgs.starship nixpkgs.zoxide nixpkgs.lazydocker nixpkgs.fastfetch
 
 nix-env -iA nixpkgs.python39 nixpkgs.nodejs_23 nixpkgs.php84 nixpkgs.php84Packages.composer
 

0d49c7dc36c596573b5465a4bd446ba275f6ad8c
tlauffs <timlauffs@gmail.com>
2025-02-09 01:34:01 +0100

    install script

--- install/install_nix_pkgs.sh
+++ install/install_nix_pkgs.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
+nix-channel --update
 nix-env -iA nixpkgs.neovim nixpkgs.fish nixpkgs.starship nixpkgs.zoxide nixpkgs.lazydocker

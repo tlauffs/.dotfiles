99d86e426194b8a8b26485524d2447b39415c6f0
tlauffs <timlauffs@gmail.com>
2025-04-06 18:14:33 +0200

    hyprland config changes

--- install/setup_hyprland_arch.sh
+++ install/setup_hyprland_arch.sh
@@ -3,6 +3,6 @@
 sudo pacman -Syu 
 sudo pacman -S hyprland kitty thunar rofi-wayland waybar hyprpaper
 # must have utils
-sudo pacman -S dunst pipewire xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland cliphist
+sudo pacman -S dunst pipewire xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland cliphist network-manager-applet
 
 

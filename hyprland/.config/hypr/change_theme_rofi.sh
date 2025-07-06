80d4711ca80b0f944f376c30fdf7e8b158870f6e
tlauffs <timlauffs@gmail.com>
2025-07-06 17:16:01 +0200

    big big big big changes

--- hyprland/.config/hypr/change_theme_rofi.sh
+++ hyprland/.config/hypr/change_theme_rofi.sh
@@ -1,11 +1,34 @@
 #!/usr/bin/env bash
 
-echo -e "rose-pine    <span foreground='#191724'>●</span> <span foreground='#e0def4'>●</span> <span foreground='#c4a7e7'>●</span> <span foreground='#f6c177'>●</span>\n\
-catppuccin   <span foreground='#1e1e2e'>●</span> <span foreground='#cdd6f4'>●</span> <span foreground='#89b4fa'>●</span> <span foreground='#f38ba8'>●</span>\n\
-gruvbox      <span foreground='#1d2021'>●</span> <span foreground='#fbf1c7'>●</span> <span foreground='#83a598'>●</span> <span foreground='#d79921'>●</span>\n\
-kanagawa     <span foreground='#1F1F28'>●</span> <span foreground='#DCD7BA'>●</span> <span foreground='#7E9CD8'>●</span> <span foreground='#E46876'>●</span>\n\
-digital-rain <span foreground='#000000'>●</span> <span foreground='#00FF00'>●</span> <span foreground='#5482AF'>●</span> <span foreground='#64C83C'>●</span>"\
-| rofi -dmenu -markup-rows -p "Select Theme" \
-| awk '{print $1}' \
-| xargs ~/.local/bin/apply-theme
+themes_dir="$HOME/.dotfiles/themes"
+themes=()
+max_len=0
 
+# Find the maximum length of the theme names
+for theme_file in "$themes_dir"/*.sh; do
+    theme_name=$(basename "$theme_file" .sh)
+    if ((${#theme_name} > max_len)); then
+        max_len=${#theme_name}
+    fi
+done
+
+while IFS= read -r theme_file; do
+    theme_name=$(basename "$theme_file" .sh)
+    source "$theme_file"
+    
+    colors="<span foreground='$THEME_BASE08'>●</span> <span foreground='$THEME_BASE09'>●</span> <span foreground='$THEME_BASE0A'>●</span> <span foreground='$THEME_BASE0B'>●</span> <span foreground='$THEME_BASE0C'>●</span> <span foreground='$THEME_BASE0D'>●</span> <span foreground='$THEME_BASE0E'>●</span>"
+    
+    # Pad the theme name with spaces for alignment
+    padded_name=$(printf "%-${max_len}s" "$theme_name")
+
+    themes+=("$padded_name  $colors")
+done < <(find "$themes_dir" -maxdepth 1 -type f -name "*.sh")
+
+theme_choice=$(for theme in "${themes[@]}"; do
+    echo -e "$theme"
+done | rofi -dmenu -markup-rows -p "Select Theme")
+
+if [ -n "$theme_choice" ]; then
+    selected_theme_name=$(echo "$theme_choice" | awk '{print $1}')
+    ~/.local/bin/apply-theme "$selected_theme_name"
+fi

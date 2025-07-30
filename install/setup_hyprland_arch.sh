bd31e6f2d0a552c123544b539adde49538b84b06
tlauffs <timlauffs@gmail.com>
2025-07-30 20:13:34 +0200

    123

--- install/setup_hyprland_arch.sh
+++ install/setup_hyprland_arch.sh
@@ -3,7 +3,7 @@
 sudo pacman -Syu 
 sudo pacman -S hyprland kitty thunar rofi-wayland waybar swww hyprlock gnome-keyring libsecret gnome-themes-extra papirus-icon-theme
 # must have utils
-sudo pacman -S dunst pipewire xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland cliphist network-manager-applet brightnessctl
+sudo pacman -S dunst pipewire xdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland cliphist network-manager-applet brightnessctl grim slurp noto-fonts-emoji
 
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'

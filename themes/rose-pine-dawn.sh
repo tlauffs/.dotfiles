347838e69bfe3515bdcad4ed44bec92d6a8d12a2
tlauffs <timlauffs@proton.me>
2025-11-21 20:48:34 +0100

    balls

---
+++ themes/rose-pine-dawn.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Base16 Rosé Pine
+# Scheme: Rosé Pine by Emilia Dunfelt &lt;sayhi@emiliadunfelt.com&gt;
+
+# export THEME_BASE00="#faf4ed"
+# export THEME_BASE01="#fffaf3"
+# export THEME_BASE02="#f2e9de"
+export THEME_BASE00="#eee7df"
+# export THEME_BASE00="#e8e0d8"
+export THEME_BASE01="#f4ede6"
+export THEME_BASE02="#e6dbd2"
+export THEME_BASE03="#9893a5"
+export THEME_BASE04="#797593"
+export THEME_BASE05="#575279"
+export THEME_BASE06="#575279"
+export THEME_BASE07="#cecacd"
+export THEME_BASE08="#b4637a"
+export THEME_BASE09="#ea9d34"
+export THEME_BASE0A="#d7827e"
+export THEME_BASE0B="#286983"
+export THEME_BASE0C="#56949f"
+export THEME_BASE0D="#907aa9"
+export THEME_BASE0E="#ea9d34"
+export THEME_BASE0F="#cecacd"
+
+# Wallpaper Directory
+export WALLPAPER_DIR="$HOME/wallpapers/rose-pine-dawn/"
+
+# Font Family
+export FONT_FAMILY="JetBrainsMono Nerd Font"
+
